ed3c1cd831a2bd2ad394e1079b4a7e02be78a093
Rajaram Karki <rajaramkarki89@gmail.com>
2023-09-29 20:50:13 +0545

    Added Server Side Code
    Added a simple localhost server in python.
    
    Co-Authored-By: E.N.D <57499980+E-n-N-D@users.noreply.github.com>
    Co-Authored-By: AvishekKC01 <67748368+AvishekKC01@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,1 +1,33 @@
 #server side code
+
+import flwr as fl
+import sys
+import numpy as np
+from typing import List
+
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(
+        self,
+        rnd,
+        results,
+        failures
+    ):
+        aggregated_weights, _ = super().aggregate_fit(rnd, results, failures)
+        if aggregated_weights is not None:
+
+            weights: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_weights)
+            # Save aggregated_weights
+            print(f"Saving round {rnd} aggregated_weights...")
+            np.savez(f"round-{rnd}-weights.npz", *weights)
+        return aggregated_weights, _
+
+# Creating strategy and run server
+strategy = SaveModelStrategy()
+
+# Starting Flower server for 10 rounds of federated learning
+fl.server.start_server(
+        server_address = 'localhost:'+str(sys.argv[1]) , 
+        config=fl.server.ServerConfig(num_rounds=10) ,
+        grpc_max_message_length = 1024*1024*1024,
+        strategy = strategy
+)
