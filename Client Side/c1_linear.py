81344e27338854107101a3f902c8082bc19eb50a
Rajaram Karki <rajaramkarki89@gmail.com>
2023-09-30 20:27:10 +0545

    Changed Client Side Code, Added different Data Files
    Changed Client Side Code to show the client data as well in streamlite along with a chart. And made a separate different data files for each client.

--- Client Side/c1_linear.py
+++ Client Side/c1_linear.py
@@ -1,7 +1,7 @@
 import flwr as fl
 import tensorflow as tf
-import pandas as pd
 import sys
+import pandas as pd
 import streamlit as st
 
 tf.random.set_seed(1)
@@ -13,41 +13,53 @@ model = tf.keras.Sequential([
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # Loading dataset
-client = 1
-num_data = 10000
-df = pd.read_csv("./cardio_train.csv", sep=";")
-Y = df['cardio']
-X = df.drop(columns=['cardio','id'], axis=1)
-x_train = X[(client-1)*10000:client*10000]
-x_test = X[50000:]
-y_train = Y[(client-1)*10000:client*10000]
-y_test = Y[50000:]
+df = pd.read_csv("./client1.csv")
+y_train = df['cardio']
+x_train = df.drop(columns=['cardio','id'], axis=1)
+
+df_test =  pd.read_csv("./test.csv")
+y_test = df_test['cardio']
+x_test = df_test.drop(columns=['cardio','id'], axis=1)
 
 # Create a Streamlit app
-st.title("server Side Accuracy Data")
+st.markdown("<h1><b>Project DAML:</b></h1>", unsafe_allow_html=True)
 
 # Define Flower client
 class FlowerClient(fl.client.NumPyClient):
-    def get_parameters(self,config):
+    def get_parameters(self, config):
         return model.get_weights()
 
     def fit(self, parameters, config):
         model.set_weights(parameters)
         r = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test), batch_size=32, verbose=0)
-        # hist = r.history
-        # st.write("Fit history : " ,hist)
+        hist = r.history
+
+        filtered_hist = {key: value for key, value in hist.items() if key in ["loss", "accuracy"]}
+
+        # Display "Client-Side Training History:" in bold and larger size
+        st.markdown("<h3><b>Client-Side Training History:</b></h3>", unsafe_allow_html=True)
+
+        for key, values in filtered_hist.items():
+            st.write(f"{key}: {values}")
+
+            if key == "accuracy":
+                st.write("Plotting Accuracy Values:")
+                chart = st.line_chart(values)
+
         return model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
         model.set_weights(parameters)
         loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
-        st.write("Eval accuracy:", accuracy)  # Display accuracy using Streamlit
+        st.markdown("<h3><b>Global Server Accuracy:</b></h3>", unsafe_allow_html=True)
+        st.write("Global accuracy:", accuracy)  # Display global server accuracy using Streamlit
         return loss, len(x_test), {"accuracy": accuracy}
 
 # Starting Flower client
 st.write("Connecting to the server...")
 fl.client.start_numpy_client(
-        server_address="localhost:"+str(sys.argv[1]), 
-        client=FlowerClient(), 
-        grpc_max_message_length=1024*1024*1024
+    # server_address="192.168.11.66:"+str(sys.argv[1]), 
+    server_address="localhost:"+str(sys.argv[1]), 
+    client=FlowerClient(), 
+    grpc_max_message_length=1024*1024*1024
 )
